beafa1724fc0c0911c70c8792ee95c08fdb16527
Blankwhiter <belonghuang@126.com>
2018-05-22 22:56:54 +0800

    配置缓存

--- src/main/java/com/example/demo/WeatherDemoApplication.java
+++ src/main/java/com/example/demo/WeatherDemoApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching//增加注解@EnableCaching，开启缓存功能 与redis配合
 public class WeatherDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/controller/indexController.java
+++ src/main/java/com/example/demo/controller/indexController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.WeatherDataService;
+import com.example.demo.vo.WeatherResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,22 @@ public class indexController {
     @RequestMapping("/index")
     public String indexTest(){
         return  "index" ;
+    }
+
+    @Autowired
+    private WeatherDataService weatherDataService;
+
+//    @GetMapping("/cityId/{cityId}") 由于tomcat高版本对url 有严格的要求 故不适用
+    @GetMapping("/cityId")
+//    public WeatherResponse getWeatherByCityId(@PathVariable("cityId") String cityId) {
+    public WeatherResponse getWeatherByCityId(String cityId) {
+        return weatherDataService.getDataByCityId(cityId);
+    }
+
+    @GetMapping("/cityName")
+//    @GetMapping("/cityName/{cityName}")
+//    public WeatherResponse getWeatherByCityName(@PathVariable("cityName") String cityName) {
+    public WeatherResponse getWeatherByCityName(String cityName) {
+        return weatherDataService.getDataByCityName(cityName);
     }
 }
