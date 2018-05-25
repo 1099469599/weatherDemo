769b28f6bc045c740a79cbce33c04166f13ea905
Blankwhiter <belonghuang@126.com>
2018-05-25 13:44:07 +0800

    搭建定时任务 获取天气数据 并放置进redis

--- src/main/java/com/example/demo/WeatherDemoApplication.java
+++ src/main/java/com/example/demo/WeatherDemoApplication.java
@@ -3,9 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 //@EnableCaching//增加注解@EnableCaching，开启缓存功能 与redis配合
+@EnableDiscoveryClient //设置为客户端
+@EnableFeignClients//feign 服务消费者 配置
 public class WeatherDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/controller/indexController.java
+++ src/main/java/com/example/demo/controller/indexController.java
@@ -1,12 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.CityDataService;
 import com.example.demo.service.WeatherDataService;
+import com.example.demo.vo.City;
 import com.example.demo.vo.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by hbl on 2018/5/22.
@@ -22,6 +26,9 @@ public class indexController {
     @Autowired
     private WeatherDataService weatherDataService;
 
+    @Autowired
+    private CityDataService cityDataService;
+
 //    @GetMapping("/cityId/{cityId}") 由于tomcat高版本对url 有严格的要求 故不适用
     @GetMapping("/cityId")
 //    public WeatherResponse getWeatherByCityId(@PathVariable("cityId") String cityId) {
@@ -29,10 +36,22 @@ public class indexController {
         return weatherDataService.getDataByCityId(cityId);
     }
 
-    @GetMapping("/cityName")
+    @RequestMapping("/cityName")
 //    @GetMapping("/cityName/{cityName}")
 //    public WeatherResponse getWeatherByCityName(@PathVariable("cityName") String cityName) {
     public WeatherResponse getWeatherByCityName(String cityName) {
         return weatherDataService.getDataByCityName(cityName);
     }
+
+    @RequestMapping("/cityNamePath/{cityName}")
+    public WeatherResponse getWeatherByCityNamePath(@PathVariable("cityName") String cityName) {
+        return weatherDataService.getDataByCityName(cityName);
+    }
+
+    @GetMapping("/citys")
+    public List<City> getWeatherByCityName() throws Exception {
+        return  cityDataService.listCity();
+    }
+
+
 }
